de26459980e6b3f0aebcf40f6ccc2b1707286952
bayanasser111 <bayanasser111@gmail.com>
2026-04-22 15:06:18 +0300

    Update Solution2_HashSet.java

--- Solution2_HashSet.java
+++ Solution2_HashSet.java
@@ -1,1 +1,18 @@
+import java.util.HashSet;
 
+public class BookInquiryHashSet {
+
+    HashSet<String> books = new HashSet<>();
+
+    public void addBook(String bookName) {
+        books.add(bookName);
+    }
+
+    public boolean searchBook(String bookName) {
+        return books.contains(bookName);
+    }
+
+    public void displayBooks() {
+        System.out.println("Available Books: " + books);
+    }
+}
