e16c7b82fceae233e5db13895100988f83c4001b
Wajd AlShehri <128199621+I9797l@users.noreply.github.com>
2026-04-26 02:27:09 +0300

    Update BookInquiryHashSet.java

--- BookInquiryHashSet.java
+++ BookInquiryHashSet.java
@@ -2,16 +2,21 @@ import java.util.HashSet;
 
 public class BookInquiryHashSet {
 
+    // HashSet is used to store unique book titles
     HashSet<String> books = new HashSet<>();
 
+    // This method adds a new book to the collection
     public void addBook(String bookName) {
         books.add(bookName);
     }
 
+    // This method checks if a book exists in the HashSet
+    // It uses contains() which is very fast (O(1) on average)
     public boolean searchBook(String bookName) {
-        return books.contains(bookName);
+        return books.contains(bookName);    // Returns true if found, false otherwise
     }
 
+    // This method displays all available books in the system
     public void displayBooks() {
         System.out.println("Available Books: " + books);
     }
